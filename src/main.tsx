16deddf9a5c876fdce460ced44d26eb301f632b2
Pedro Affonseca <pedroaffonseca.ufmg@gmail.com>
2026-07-23 16:44:57 -0300

    fix: altera o main para ter varias abas ao inves de apenas app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import ClientsPage from './pages/ClientsPage'
+import ServiceOrdersPage from './pages/ServiceOrdersPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "clients", element: <ClientsPage /> },
+      { path: "service-orders", element: <ServiceOrdersPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
